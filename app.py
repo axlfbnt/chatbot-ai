8db082a3ef627dc673d4873e455a682401130e0d
Axel Fabianto <fabiantoaxel042@gmail.com>
2025-05-21 12:53:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ API_URL = "https://openrouter.ai/api/v1/chat/completions"
 # Tampilan halaman
 st.set_page_config(page_title="AI Chatbot", layout="centered")
 st.title("🧠 AI Chatbot Bubble Style")
-st.markdown(f"Powered by `{MODEL}` via OpenRouter 🤖 — <span style='font-size: 0.5em;'>explored & curated by <b>Axel</b></span>", unsafe_allow_html=True)
+st.markdown(f"Powered by `{MODEL}` via OpenRouter 🤖 — <span style='font-size: 0.7em;'>explored & curated by <b>Axel</b></span>", unsafe_allow_html=True)
 
 # Tombol New Chat
 if st.button("🔄 New Chat"):
